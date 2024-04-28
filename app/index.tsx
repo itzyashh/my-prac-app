27b8db55cdec9c7a0533a528f4dfe9b12ceebd44
Yash Jadhav <yashjadhav1411@rocketmail.com>
2024-04-28 17:26:24 +0530

    add basic navigation

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text>index</Text>
+      <Link href={'/protected'}>Go to Protected</Link>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
---
+++ app/protected/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Protected = () => {
+  return (
+    <View>
+      <Text>Protected</Text>
+    </View>
+  )
+}
+
+export default Protected
+
+const styles = StyleSheet.create({})
